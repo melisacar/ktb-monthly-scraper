eb4542ad074537d9f179f6d0c7970b8916a1ea8d
Melisa <acarme17@itu.edu.tr>
2024-11-01 14:23:59 +0300

    ft: pdf parser func

--- src/main_ktb_scraper.py
+++ src/main_ktb_scraper.py
@@ -7,18 +7,18 @@ import pandas as pd
 
 # Map month numbers.
 months_mapping = {
-    1: "2024 Yılı Ocak Ayı",
-    2: "2024 Yılı Şubat Ayı",
-    3: "2024 Yılı Mart Ayı",
-    4: "2024 Yılı Nisan Ayı",
-    5: "2024 Yılı Mayıs Ayı",
-    6: "2024 Yılı Haziran Ayı",
-    7: "2024 Yılı Temmuz Ayı",
-    8: "2024 Yılı Ağustos Ayı",
-    9: "2024 Yılı Eylül Ayı",
-    10: "2024 Yılı Ekim Ayı",
-    11: "2024 Yılı Kasım Ayı",
-    12: "2024 Yılı Aralık Ayı"
+    1: "Ocak",
+    2: "Şubat",
+    3: "Mart",
+    4: "Nisan",
+    5: "Mayıs",
+    6: "Haziran",
+    7: "Temmuz",
+    8: "Ağustos",
+    9: "Eylül",
+    10: "Ekim",
+    11: "Kasım",
+    12: "Aralık"
 }
 
 def disable_ssl_warnings():
@@ -42,46 +42,47 @@ def fetch_page_content(url):
 
 def parse_excel_links(html_content):
     """
-    Parses the HTML content to find all Excel file links.
+    Parses the HTML content to find all PDF file links.
     Returns the last href found (the most recent one).
     """
     soup = BeautifulSoup(html_content, "html.parser")
     # Find the specific article class.
-    article = soup.find('article', class_='art_loop')
-    if not article:
-        print("No article with class 'art_loop' found.")
+    links = soup.find('a', style='font-family: Georgia; font-size: 14px;')
+    
+    if not links:
+        print("No link with class 'font-family: Georgia; font-size: 14px;' found.")
         return None
     
     elements = soup.find_all(
-        'a'#,
-        #class_="art_loop"
+        'a'
     )
-    hrefs = [element.get('href') for element in elements if element.get('href') and '.xls' in element.get('href')]
+    hrefs = [element.get('href') for element in elements if element.get('href') and '.pdf' in element.get('href')]
      # If hrefs list empty, return None
     if not hrefs:
         print("No links found.")
         return None
-    return hrefs[-1]            ##### Returns only the last href.!!!!
+    return hrefs[-1]            ##### Returns only the last href.!!!! Here is Ocak 2024
 
 
 # Add to main function at the end.
-url = "https://yigm.ktb.gov.tr/TR-366207/2024.html"
+url = "https://istanbul.ktb.gov.tr/TR-368430/istanbul-turizm-istatistikleri---2024.html"
 
 html_content = fetch_page_content(url)
 hrefs = parse_excel_links(html_content)
+
 # Control
-# print(hrefs)
+print(hrefs)
 ####################################################################
     # HTML Control.
 #if html_content:
-#    print(html_content.decode('utf-8'))  # HTML içeriğini yazdır
+#    print(html_content.decode('utf-8'))  # HTML content.
 #else:
 #    print("Failed to fetch HTML content.")
 
     # Control.
 #if html_content:
 #    hrefs = parse_excel_links(html_content)
-#    print(hrefs)  # Hrefs listesini yazdır
-#else:
+#    print(hrefs)  # Hrefs list.
+# else:
 #    print("Failed to fetch HTML content.")
 ####################################################################

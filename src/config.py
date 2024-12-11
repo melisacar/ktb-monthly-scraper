f579f8d3d61c9ca2c100208a952ebc727f1073d7
Melisa <acarme17@itu.edu.tr>
2024-12-11 16:39:25 +0300

    ch: change on filename to config

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Database connection
+database_url = 'postgresql+psycopg2://postgres:secret@database:5432/ktb-scrape'
+
+# Create the engine
+engine = create_engine(database_url)
+
+# Create a session for database operations.
+Session = sessionmaker(bind=engine)

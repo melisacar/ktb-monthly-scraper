75c27bc1e1018bb9a80c82d8ddd7593a61652eff
Melisa <acarme17@itu.edu.tr>
2024-12-12 16:18:02 +0300

    ft: change on logic needs edit

--- src/main_checker.py
+++ src/main_checker.py
@@ -10,6 +10,11 @@ from urllib.parse import urljoin
 from tabulate import tabulate
 import pdfplumber
 import pandas as pd
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import extract
+from models import gelen_yabanci_ziyaretci, engine
+from datetime import datetime
+from sqlalchemy.exc import IntegrityError
 
 # Map month numbers
 months_mapping = {
@@ -78,12 +83,22 @@ def get_year_from_page(pdf, page_number):
                 return year  
     return None
 
+def check_month_and_year_exists(session, month, year):
+    exists = (
+        session.query(gelen_yabanci_ziyaretci)
+        .filter(
+            extract('month', gelen_yabanci_ziyaretci.tarih) == month,
+            extract('year', gelen_yabanci_ziyaretci.tarih == year)
+        )
+        .first()
+    )
+    return exists is not None
 
-def read_pdf_from_url(hrefs, base_url, target_month):
+def read_pdf_from_url(href, base_url):
     """
     Reads the PDF content from the given href and extracts the first row containing the target month.
     """
-    full_url = urljoin(base_url, hrefs)
+    full_url = urljoin(base_url, href)
     response = requests.get(full_url, verify=False)
 
     if response.status_code == 200:
@@ -98,60 +113,112 @@ def read_pdf_from_url(hrefs, base_url, target_month):
                 print("Year not found on the specified page.")
                 return
             
-            month_name = months_mapping.get(target_month, "").upper()
-            month_found = False
+            latest_month = find_newest_month_html(pdf)
+            if not latest_month:
+                print("Could not find the latest month in the PDF.")
+                return
+            
+            for month_num in range(latest_month, 0, -1):  # Start from the latest month
+                month_name = months_mapping[month_num].upper()
+                month_found = False
 
-
-            for page in pdf.pages:
-                page_number = 3 # First needed table on this page.
-                table_one_page = pdf.pages[page_number]
-                table_one_txt = table_one_page.extract_text_simple()
-                #print(table_one_txt)
-                if table_one_txt:
-                    for line in table_one_txt.split('\n'):
-                        if month_name in line.upper():
-                            #print(f"Ay {month_name} içeren ilk satır:\n{line}")
-                            month_found = True
-                            
-                            value = line.split()
-                            try:
-                                turkiye_value = value[3]
-                                istanbul_value = value[7]
-                                # Format and print the final output in YYYY-MM-01 format
-                                date_prefix = f"{year}-{target_month:02d}-01"
-                                output.append([date_prefix, "Türkiye", turkiye_value])
-                                output.append([date_prefix, "İstanbul", istanbul_value])
-                            
-                            except IndexError:
-                                 print("Beklenen pozisyonda veri bulunamadı. Lütfen satır yapısını kontrol edin.")
-                            
-                            break  
-                if month_found:
-                    break
-            if not month_found:
-                print(f"Ay {month_name} PDF'de bulunamadı.")
+                for page in pdf.pages:
+                    table_one_txt = page.extract_text_simple()
+                    if table_one_txt:
+                        for line in table_one_txt.split('\n'):
+                            if month_name in line.upper():
+                                month_found = True
+                                value = line.split()
+                                try:
+                                    turkiye_value = value[3]
+                                    istanbul_value = value[7]
+                                    date_prefix = f"{year}-{month_num:02d}-01"
+                                    output.append([date_prefix, "Türkiye", turkiye_value])
+                                    output.append([date_prefix, "İstanbul", istanbul_value])
+                                except IndexError:
+                                    print(f"Data not found in expected position for {month_name}. Line: {line}")
+                                break
+                    if month_found:
+                        break
+                if not month_found:
+                    print(f"{month_name} not found in PDF.")
     else:
         print(f"PDF alınamadı {full_url}, durum kodu: {response.status_code}") 
     df = pd.DataFrame(output,  columns=["tarih", "ist_tr", "ziyaretci_sayisi"])
     return df
 
-def main_check():
+def save_to_database(df, session):
+    for _, row in df.iterrows():
+        new_record = gelen_yabanci_ziyaretci(
+            tarih = row["tarih"],
+            ist_tr = str(row["ist_tr"]),
+            ziyaretci_sayisi = float(row["ziyaretci_sayisi"]),
+            erisim_tarihi = datetime.today().strftime("%Y-%m-%d")
+        )
+        try:
+            session.add(new_record)
+            session.commit()
+        except IntegrityError:
+            session.rollback()
+    print("Data added to the database.")
+
+def generate_date_prefix(year, month):
+    """
+    Creates a date prefix in the format YYYY-MM-01.
+    """
+    return f"{year}-{month:02d}-01"
+
+def extract_year_month(date_info):
+    months_mapping = {
+    1: "Ocak", 2: "Şubat", 3: "Mart", 4: "Nisan", 5: "Mayıs", 6: "Haziran", 
+    7: "Temmuz", 8: "Ağustos", 9: "Eylül", 10: "Ekim", 11: "Kasım", 12: "Aralık"
+    }
+    parts = date_info.split()
+    year = parts[0]
+    month_text = parts[1]
+    month = months_mapping.get(month_text, "00")
+
+    return f"{year}-{month}-01"
+
+
+def main_02_01():
     # Main script execution
-    url = "https://istanbul.ktb.gov.tr/TR-368430/istanbul-turizm-istatistikleri---2024.html"
-    base_url = "https://istanbul.ktb.gov.tr"
+    
+    Session = sessionmaker(bind=engine)
+    session = Session
+
     disable_ssl_warnings()
 
-    html_content = fetch_page_content(url)
-    if html_content:
-        hrefs = parse_pdf_links(html_content)
-        newest_month = find_newest_month_html(html_content)
-        
-        if hrefs and newest_month:
-            df = read_pdf_from_url(hrefs, base_url, newest_month)
-            print(df)
-        else:
-            print("No PDF link found or no valid month found.")
-    else:
-        print("Failed to fetch HTML content.")
+    url = "https://istanbul.ktb.gov.tr/TR-368430/istanbul-turizm-istatistikleri---2024.html"
+    base_url = "https://istanbul.ktb.gov.tr"
 
-main_check()
+    html_content = fetch_page_content(url)
+    if not html_content:
+        print(f"PDF file could not be downloaded: {url}")
+        return
+    href = parse_pdf_links(html_content)
+    newest_month = find_newest_month_html(html_content)
+
+    # Read the PDF and extract year and month
+    df = read_pdf_from_url(href, base_url)
+    if df is not None and not df.empty:
+        for _, row in df.iterrows():
+            parsed_date = datetime.strptime(row["tarih"], "%Y-%m-%d")
+            year = parsed_date.year
+            month = parsed_date.month
+
+            # Generate date_prefix
+            date_prefix = generate_date_prefix(year, month)
+
+            # Check if data for this date_prefix exists in the database
+            if check_month_and_year_exists(session, month, year):
+                print(f"Record already exists for {date_prefix}. Skipping...")
+            else:
+                # Save the data to the database
+                print(f"New data found for {date_prefix}, adding to database...")
+                save_to_database(df, session)
+    else:
+        print("No data extracted from the PDF.")
+    session.close()
+
+main_02_01()

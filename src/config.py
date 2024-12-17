4e7b0af70ab17d80e6f086013bd3751568b2a3f6
Melisa <acarme17@itu.edu.tr>
2024-12-17 16:52:36 +0300

    ft: switch on db url

--- src/config.py
+++ src/config.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Database connection
-database_url = 'postgresql+psycopg2://postgres:secret@database:5432/ktb-scrape'
+#database_url = 'postgresql+psycopg2://postgres:secret@database:5432/ktb-scrape'
+DATABASE_URL = 'postgresql+psycopg2://postgres:secret@localhost:5432/ktb-scrape'
 
 # Create the engine
-engine = create_engine(database_url)
+engine = create_engine(DATABASE_URL)
 
 # Create a session for database operations.
 Session = sessionmaker(bind=engine)

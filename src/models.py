264e818651237b27cbaf3dd5033261ad2c8a5322
Melisa <acarme17@itu.edu.tr>
2024-12-17 16:18:15 +0300

    feat: add unique constraints to prevent duplicate data

--- src/models.py
+++ src/models.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, Date, String, Float
 from config import engine 
+from sqlalchemy import UniqueConstraint
 
 Base = declarative_base()
 
 # Define the gelen;_yabanci_ziyaretci table
 class gelen_yabanci_ziyaretci(Base):
     __tablename__ = 'gelen_yabanci_ziyaretci'
-    __table_args__ = {'schema': 'etl'}
+    __table_args__ = (UniqueConstraint('tarih', 'ist_tr', name = 'unique_gelen_yabanci_ziyaretci'),
+        {'schema': 'etl'}
+    )
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     tarih = Column(Date, nullable=True)
